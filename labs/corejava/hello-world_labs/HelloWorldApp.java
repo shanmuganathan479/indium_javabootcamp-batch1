1de740575a149fc904e95c84fa9854f4581ad8ed
Shanmuganathan -Indium <shanmuganathan.selvakumar@indiumsoft.com>
2023-08-25 18:40:06 +0530

    Hello World

---
+++ labs/corejava/hello-world_labs/HelloWorldApp.java
@@ -0,0 +1,12 @@
+/**
+* First Java Program Demo
+*/
+public class HelloWorldApp {
+	// main method
+	public static void main(String[] args) {
+		/* print statement
+		* demo
+      */
+		System.out.println("Hello World!!!");
+	}
+}
