eeec012484f4d40dbe581f16aace40a8378bacf5
Shanmuganathan -Indium <shanmuganathan.selvakumar@indiumsoft.com>
2023-08-28 17:18:34 +0530

    Updated

--- labs/corejava/hello-world_labs/HelloWorldApp.java
+++ labs/corejava/hello-world_labs/HelloWorldApp.java
@@ -2,11 +2,20 @@
 * First Java Program Demo
 */
 public class HelloWorldApp {
-	// main method
-	public static void main(String[] args) {
-		/* print statement
-		* demo
+    // main method
+    public static void main(String[] args) {
+        /* print statement
+        * demo
       */
-		System.out.println("Hello World!!!");
-	}
+        System.out.println("Hello World!!!");
+
+ 
+
+        try {
+            // sleep for 5 mins
+            Thread.sleep(1000*60*5);
+        }catch(InterruptedException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
